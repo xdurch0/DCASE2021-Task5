3a46225ae00530bc9283d44c933e3395cddbdb32
xdurch0 <j_johannsmeier@gmx.de>
2021-03-24 13:32:17 +0100

    refactored functions into two files

--- data/preparation.py
+++ data/preparation.py
@@ -2,18 +2,17 @@
 
 """
 import os
-import warnings
 from glob import glob
 from itertools import chain
-from typing import Tuple, Union, Optional
+from typing import Tuple, Optional
 
 import h5py
-import librosa
 import numpy as np
 import pandas as pd
-import scipy
 from omegaconf import DictConfig
-from scipy.io import wavfile
+
+from .transforms import (resample_audio, RawExtractor, FeatureExtractor,
+                         extract_feature)
 
 pd.options.mode.chained_assignment = None
 
@@ -192,158 +191,7 @@ def create_dataset(df_events: pd.DataFrame,
     return label_list
 
 
-class RawExtractor:
-    def __init__(self):
-        pass
 
-    @staticmethod
-    def extract_feature(audio: np.ndarray) -> np.ndarray:
-        return audio[:, None].astype(np.float32)
-
-
-class FeatureExtractor:
-    def __init__(self,
-                 conf: DictConfig):
-        self.sr = conf.features.sr
-        self.n_fft = conf.features.n_fft
-        self.hop = conf.features.hop_mel
-        self.n_mels = conf.features.n_mels
-        self.fmax = conf.features.fmax
-        self.type = conf.features.type
-        self.time_constant = conf.features.time_constant
-        self.pcen_power = conf.features.power
-        self.bias = conf.features.bias
-        self.gain = conf.features.gain
-
-    def extract_feature(self,
-                        audio: np.ndarray) -> np.ndarray:
-        audio *= 2**31  # TODO check if this actually matters (prob. not lol)
-
-        mel_spec = librosa.feature.melspectrogram(audio,
-                                                  sr=self.sr,
-                                                  n_fft=self.n_fft,
-                                                  hop_length=self.hop,
-                                                  n_mels=self.n_mels,
-                                                  fmax=self.fmax,
-                                                  power=1)
-        if self.type == "pcen":
-            features = librosa.core.pcen(mel_spec,
-                                         sr=self.sr,
-                                         hop_length=self.hop,
-                                         time_constant=self.time_constant,
-                                         power=self.pcen_power,
-                                         bias=self.bias,
-                                         gain=self.gain)
-        elif self.type == "pcen_lowpass":
-            features = pcen_lowpass(mel_spec,
-                                    sr=self.sr,
-                                    hop_length=self.hop,
-                                    time_constant=self.time_constant)
-            features = np.concatenate([mel_spec, features], axis=0)
-        elif self.type == "logmel":
-            features = np.log(mel_spec**2 + 1e-8)
-        else:
-            raise ValueError("Invalid type {} in "
-                             "FeatureExtractor".format(self.type))
-        features = features.T.astype(np.float32)
-
-        return features
-
-
-def pcen_lowpass(S, sr=22050, hop_length=512, time_constant=0.400, b=None,
-                 max_size=1, ref=None, axis=-1, max_axis=None, zi=None):
-
-    if time_constant <= 0:
-        raise ValueError('time_constant={} must be strictly '
-                         'positive'.format(time_constant))
-
-    if max_size < 1 or not isinstance(max_size, int):
-        raise ValueError('max_size={} must be a positive '
-                         'integer'.format(max_size))
-
-    if b is None:
-        t_frames = time_constant * sr / float(hop_length)
-        # By default, this solves the equation for b:
-        #   b**2  + (1 - b) / t_frames  - 2 = 0
-        # which approximates the full-width half-max of the
-        # squared frequency response of the IIR low-pass filter
-
-        b = (np.sqrt(1 + 4 * t_frames**2) - 1) / (2 * t_frames**2)
-
-    if not 0 <= b <= 1:
-        raise ValueError('b={} must be between 0 and 1'.format(b))
-
-    if np.issubdtype(S.dtype, np.complexfloating):
-        warnings.warn('pcen was called on complex input so phase '
-                      'information will be discarded. To suppress this warning, '
-                      'call pcen(np.abs(D)) instead.')
-        S = np.abs(S)
-
-    if ref is None:
-        if max_size == 1:
-            ref = S
-        elif S.ndim == 1:
-            raise ValueError('Max-filtering cannot be applied to 1-dimensional '
-                             'input')
-        else:
-            if max_axis is None:
-                if S.ndim != 2:
-                    raise ValueError('Max-filtering a {:d}-dimensional '
-                                     'spectrogram requires you to specify '
-                                     'max_axis'.format(S.ndim))
-                # if axis = 0, max_axis=1
-                # if axis = +- 1, max_axis = 0
-                max_axis = np.mod(1 - axis, 2)
-
-            ref = scipy.ndimage.maximum_filter1d(S, max_size, axis=max_axis)
-
-    if zi is None:
-        # Make sure zi matches dimension to input
-        shape = tuple([1] * ref.ndim)
-        zi = np.empty(shape)
-        zi[:] = scipy.signal.lfilter_zi([b], [1, b - 1])[:]
-
-    # Temporal integration
-    S_smooth, zf = scipy.signal.lfilter([b], [1, b - 1], ref, zi=zi,
-                                        axis=axis)
-
-    return S_smooth
-
-
-def extract_feature(audio_path: str,
-                    feature_extractor: Union[FeatureExtractor, RawExtractor],
-                    conf: DictConfig) -> np.ndarray:
-    """Load audio and apply feature extractor.
-
-    Parameters:
-        audio_path: Path to audio file.
-        feature_extractor: FeatureExtractor object to... guess what.
-        conf: hydra config object.
-
-    Returns:
-        Features in shape time x features.
-
-    """
-    y, sr = librosa.load(audio_path, sr=None)
-    if sr != conf.features.sr:
-        raise ValueError("Audio should have been resampled at this stage. Found"
-                         "{}Hz, expected {}Hz.".format(sr, conf.features.sr))
-
-    y = feature_extractor.extract_feature(y)
-    return y
-
-
-def resample_audio(audio_path: str,
-                   sr: int):
-    """Resample and save a single audio file.
-
-    Parameters:
-        audio_path: Path to audio file.
-        sr: Target sampling rate.
-
-    """
-    y, _ = librosa.load(audio_path, sr=sr)
-    wavfile.write(audio_path[:-4] + "_{}hz.wav".format(sr), sr, y)
 
 
 def resample_all(conf: DictConfig):

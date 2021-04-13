38d49d9a17f256775d581ba5bb48ffab7ae5481d
xdurch0 <j_johannsmeier@gmx.de>
2021-04-13 11:40:12 +0200

    fixed import and improved code

--- analyze/metrics.py
+++ analyze/metrics.py
@@ -1,19 +1,16 @@
 import os
-from typing import Optional, Tuple
+from typing import Optional
 
 import numpy as np
 from matplotlib import pyplot as plt
 from omegaconf import DictConfig
 
-from ..evaluation_metrics.evaluation import evaluate
+from evaluation_metrics.evaluation import evaluate
 
 
 def get_classification_metrics(conf: DictConfig,
                                results_path: Optional[str] = None,
-                               n_models: Optional[int] = None) -> Tuple[list,
-                                                                        list,
-                                                                        list,
-                                                                        list]:
+                               n_models: Optional[int] = None) -> dict:
     thresholds = np.around(np.linspace(0., 1., 101), 2)
     all_precisions = []
     all_recalls = []
@@ -50,11 +47,20 @@ def get_classification_metrics(conf: DictConfig,
         all_fscores.append(fscores)
         all_maxfs.append(maxf)
 
-    return all_precisions, all_recalls, all_fscores, all_maxfs
+    metrics = {"precisions": all_precisions,
+               "recalls": all_recalls,
+               "fscores": all_fscores}
+    return metrics
 
 
-def plot_metrics(thresholds, all_precisions, all_recalls, all_fscores,
-                 all_maxfs):
+def plot_metrics(metrics, thresholds=np.around(np.linspace(0., 1., 101), 2)):
+    all_fscores = metrics["fscores"]
+    all_precisions = metrics["precisions"]
+    all_recalls = metrics["recalls"]
+
+    all_maxfs = [fscore.max() for fscore in all_fscores]
+    all_best_threshs = [thresholds[np.argmax(fscore)] for fscore in all_fscores]
+
     n_models = len(all_fscores)
 
     color = iter(plt.cm.rainbow(np.linspace(0, 1, n_models)))
@@ -97,8 +103,12 @@ def plot_metrics(thresholds, all_precisions, all_recalls, all_fscores,
         col = next(color)
         plt.plot(thresholds, all_fscores[ind], "-", c=col, label="fscore",
                  alpha=0.2)
+
         plt.plot([0, 1], [all_maxfs[ind], all_maxfs[ind]], "--",
                  label="best fscore", alpha=0.2, c=col)
+        plt.plot([all_best_threshs[ind], all_best_threshs[ind]], "--",
+                 label="best threshold", alpha=0.2, c=col)
+
     plt.plot([0.5, 0.5], [0, 1], "k-.", alpha=0.2)
 
     plt.ylim(0, 1)

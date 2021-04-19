8dacd687adba2654260fc5c77cb15c2c81a5bf5c
xdurch0 <j_johannsmeier@gmx.de>
2021-04-19 18:05:25 +0200

    add basic statistics on individual event lengths

--- analyze/statistics.py
+++ analyze/statistics.py
@@ -2,11 +2,13 @@ import os
 from glob import glob
 
 import librosa
+import numpy as np
 import pandas as pd
 from omegaconf import DictConfig
 
 
-def get_event_fractions(conf: DictConfig, subset="training"):
+def get_event_fractions(conf: DictConfig,
+                        subset: str = "training"):
     if subset == "training":
         meta_path = conf.path.train_dir
     elif subset == "validation":
@@ -32,9 +34,16 @@ def get_event_fractions(conf: DictConfig, subset="training"):
         length_total = len(audio_file) / sr
 
         event_lengths = df_pos["Endtime"] - df_pos["Starttime"]
+
         total_event_duration = 0
         for event in event_lengths:
             total_event_duration += event
+
+        max_event_duration = event_lengths.max()
+        min_event_duration = event_lengths.min()
+        mean_event_duration = event_lengths.mean()
+        event_duration_std = np.std(event_lengths)
+        median_event_duration = np.median(event_lengths)
 
         print("File {}".format(file))
         print("Number of positive events: {}".format(len(event_lengths)))
@@ -42,6 +51,12 @@ def get_event_fractions(conf: DictConfig, subset="training"):
         print("Total event duration: {}".format(total_event_duration))
         print("Event fraction: {}".format(total_event_duration / length_total))
         print()
+        print("Longest event duration: {}".format(max_event_duration))
+        print("Shortest event duration: {}".format(min_event_duration))
+        print("Mean event duration: {}".format(mean_event_duration))
+        print("Median event duration: {}".format(median_event_duration))
+        print("Event duration standard deviation: {}".format(event_duration_std))
+        print("")
 
         length_total_all_files += length_total
         total_event_duration_all_files += total_event_duration
